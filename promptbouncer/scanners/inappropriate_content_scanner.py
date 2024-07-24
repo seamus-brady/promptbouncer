f0827c18f71a7b5bf3e1f1bedce31d98906e7518
Seamus Brady <seamus@corvideon.ie>
2024-07-24 12:19:50 +0100

    Update

--- promptbouncer/scanners/inappropriate_content_scanner.py
+++ promptbouncer/scanners/inappropriate_content_scanner.py
@@ -27,7 +27,7 @@ class InappropriateContentScanner(AbstractThreatScanner):
     THREAT_SCANNER_DESC = "This scan looks for inappropriate content in a prompt."
     THREAT_LEVEL = Alarm.THREAT_SERIOUS
 
-    NOT_MODERATED = "not moderated"
+    NOT_MODERATED = "Not Moderated"
 
     @staticmethod
     def scan(prompt: str) -> Any:
@@ -63,42 +63,42 @@ class InappropriateContentScanner(AbstractThreatScanner):
                     "content": f"""
                     == INSTRUCTIONS ==
                     You're given a list of moderation categories as below:
-                    - illegal: Illegal activity.
-                    - child abuse: child sexual abuse material or any content that exploits or harms children.
-                    - hate violence harassment: Generation of hateful, harassing, or violent content: content that 
+                    - Illegal: Illegal activity.
+                    - Child Abuse: child sexual abuse material or any content that exploits or harms children.
+                    - Hate Violence Harassment: Generation of hateful, harassing, or violent content: content that 
                       expresses, incites, or promotes hate based on identity, content that intends to harass, threaten, 
                       or bully an individual, content that promotes or glorifies violence or celebrates the suffering 
                       or humiliation of others.
-                    - malware: Generation of malware: content that attempts to generate code that is designed to 
+                    - Malware: Generation of malware: content that attempts to generate code that is designed to 
                       disrupt, damage, or gain unauthorized access to a computer system.
-                    - physical harm: activity that has high risk of physical harm, including: weapons development, 
+                    - Physical Harm: activity that has high risk of physical harm, including: weapons development, 
                       military and warfare, management or operation of critical infrastructure in energy, transportation, 
                       and water, content that promotes, encourages, or depicts acts of self-harm, such as suicide, 
                       cutting, and eating disorders.
-                    - economic harm: activity that has high risk of economic harm, including: multi-level marketing, 
+                    - Economic Harm: activity that has high risk of economic harm, including: multi-level marketing, 
                       gambling, payday lending, automated determinations of eligibility for credit, employment, 
                       educational institutions, or public assistance services.
-                    - fraud: Fraudulent or deceptive activity, including: scams, coordinated inauthentic behavior, 
+                    - Fraud: Fraudulent or deceptive activity, including: scams, coordinated inauthentic behavior, 
                       plagiarism, academic dishonesty, astroturfing, such as fake grassroots support or fake review 
                       generation, disinformation, spam, pseudo-pharmaceuticals.
-                    - adult: Adult content, adult industries, and dating apps, including: content meant to arouse 
+                    - Adult: Adult content, adult industries, and dating apps, including: content meant to arouse 
                       sexual excitement, such as the description of sexual activity, or that promotes sexual services 
                       (excluding sex education and wellness), erotic chat, pornography.
-                    - political: Political campaigning or lobbying, by: generating high volumes of campaign materials, 
+                    - Political: Political campaigning or lobbying, by: generating high volumes of campaign materials, 
                       generating campaign materials personalized to or targeted at specific demographics, building 
                       conversational or interactive systems such as chatbots that provide information about campaigns 
                       or engage in political advocacy or lobbying, building products for political campaigning or 
                       lobbying purposes.
-                    - privacy: Activity that violates people's privacy, including: tracking or monitoring an 
+                    - Privacy: Activity that violates people's privacy, including: tracking or monitoring an 
                       individual without their consent, facial recognition of private individuals, classifying 
                       individuals based on protected characteristics, using biometrics for identification or 
                       assessment, unlawful collection or disclosure of personal identifiable information or 
                       educational, financial, or other protected records.
-                    - unqualified law: Engaging in the unauthorized practice of law, or offering tailored legal advice 
+                    - Unqualified Law: Engaging in the unauthorized practice of law, or offering tailored legal advice 
                       without a qualified person reviewing the information.
-                    - unqualified financial: Offering tailored financial advice without a qualified person reviewing 
+                    - Unqualified Financial: Offering tailored financial advice without a qualified person reviewing 
                       the information.
-                    - unqualified health: Telling someone that they have or do not have a certain health condition, 
+                    - Unqualified Health: Telling someone that they have or do not have a certain health condition, 
                       or providing instructions on how to cure or treat a health condition.
                     
                     Please classify the following text into one of these categories, and answer with that single 
